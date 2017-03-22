b7bea6932c570e767485a106e5ed26810c2ff080
quadrant26 <quadrant26@163.com>
2017-03-22 08:22:36 +0800

    es6 fnname.name

---
+++ docs/example/04.fn.name.js
@@ -0,0 +1,21 @@
+"use srtict";
+
+// fn => name
+function be(argument){
+
+}
+
+var breakfast = function (argument){
+
+}
+
+var superfast = function super (argument){
+
+}
+
+
+cosnole.log(
+  be.name,
+  breakfast.name,
+  superfast.name
+);
