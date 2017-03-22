da507b076911cda922c0e9d67fd21b9f3b7d00f2
quadrant26 <quadrant26@163.com>
2017-03-22 08:22:07 +0800

    es6 箭头函数

---
+++ docs/example/04.fn.arrow.js
@@ -0,0 +1,10 @@
+"use srtict";
+
+// 箭头函数
+let breakfast = dessert => dessert;
+
+let breakfast = (dessert, drink) => dessert + drink;
+
+let breakfast = (dessert, drink) => {
+    return dessert + drink
+};
