36695ce5f67efefc014fe8c448b4c66b17f0df24
quadrant26 <quadrant26@163.com>
2017-03-22 08:30:32 +0800

    es6 对象和函数 简写方式

---
+++ docs/example/05.object.literal.js
@@ -0,0 +1,15 @@
+"use srtict";
+
+// Object.literal
+// json 简写
+
+let dessert = '🎂', drink = '🍺';
+
+let food = {
+    dessert : desert,
+    drink,
+    breakfast : function (){},
+    super(){}
+}
+
+console.log(food);
