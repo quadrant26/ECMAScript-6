104e32ebc2dd26b32332d90093f876f5f74bd427
quadrant26 <quadrant26@163.com>
2017-03-22 23:51:59 +0800

    export default {}

---
+++ docs/example/09.export-default.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Module
+//
+//
+
+let fruit = '123';
+let dessert = 'abc';
+
+export default function dinner(fruit, dessert){
+    console.log(`今天的晚餐是 ${fruit} and ${dessert}`);
+}
+
+/**
+ * function dinner(fruit, dessert){
+     console.log(`今天的晚餐是 ${fruit} and ${dessert}`);
+ }
+ * export {dinner as chef}
+ */
