2e9e7b9290ac1a68d51bcba79e4d94829404d548
quadrant26 <quadrant26@163.com>
2017-03-22 23:52:31 +0800

    import {}/name/ * as nmae from file

---
+++ docs/example/09.import.js
@@ -0,0 +1,23 @@
+"use strict"
+
+import {fruit, dessert} from './09.module.js';
+
+console.log(fruit, dessert);
+
+/*
+import * as chef from './09.module.js';
+
+console.log(chef.fruit, chef.dessert);
+*/
+
+// 2
+/**
+ * import {fruit, dessert, super as dinner} from './09.module2.js';
+ * dinner(fruit, dessert);
+ */
+
+// export default
+/**
+ * import chef from './09.export-default.js''
+ * chef('ad', 'bd');
+ */
